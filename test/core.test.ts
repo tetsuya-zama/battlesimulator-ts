abd2bfd228549a6e5338fb81ee4a86f980cee35c
tetsuya-zama <tetsuya.zama@gmail.com>
2020-02-20 16:54:55 +0900

    Add comment to core.test.ts

--- test/core.test.ts
+++ test/core.test.ts
@@ -1,6 +1,12 @@
 import {IntValue, COMMAND, Fightable, Battle, CommandLog} from '../src/core';
 
+/**
+* [[IntValue]]オブジェクトについてのテスト
+*/
 describe("IntValue",()=>{
+  /**
+  * 整数によって作られる
+  */
   it("will be created by integer number",()=>{
     expect(IntValue.of(-1).value).toBe(-1);
     expect(IntValue.of(0).value).toBe(0);
@@ -8,18 +14,32 @@ describe("IntValue",()=>{
     expect(IntValue.of(Number.MAX_SAFE_INTEGER).value).toBe(Number.MAX_SAFE_INTEGER);
     expect(IntValue.of(Number.MIN_SAFE_INTEGER).value).toBe(Number.MIN_SAFE_INTEGER);
   });
+  /**
+  * IntValueそのものによっても作られる
+  */
   it("will also be created by itself",()=>{
     expect(IntValue.of(IntValue.of(-1)).value).toBe(-1);
     expect(IntValue.of(IntValue.of(0)).value).toBe(0);
     expect(IntValue.of(IntValue.of(1)).value).toBe(1);
   });
+  /**
+  * 整数以外から作ろうとするとエラーが発生する
+  */
   it("will throw Error if the value is not integer",()=>{
     expect(()=> IntValue.of(0.1)).toThrow();
     expect(()=> IntValue.of(-0.1)).toThrow();
   });
 });
 
+/**
+* [[Battle]]オブジェクトについてのテスト
+*
+* ※あくまでBattleの単体テストなのでFightableにはMockを使用している
+*/
 describe("Battle",()=>{
+  /**
+  * [[Fightable]]を２つ指定して作ることができる
+  */
   it("will be created with two Fightables",()=>{
     const result = new Battle({
       black:{
@@ -46,6 +66,9 @@ describe("Battle",()=>{
     expect(result.isFinished).toBe(false);//default value
   });
 
+  /**
+  * [[Fightable]]2つと最大ターン数を指定して作ることもできる
+  */
   it("will be created with two Fightables and max turns",()=>{
     const result = new Battle({
       black:{
@@ -73,7 +96,13 @@ describe("Battle",()=>{
     expect(result.isFinished).toBe(false);//default value
   });
 
+  /**
+  * nextTurnメソッドについてのテスト
+  */
   describe("nextTurn",()=>{
+    /**
+    * どちらかのFightableが死亡したら戦闘を終了する
+    */
     it("will finishes the battle if one of Fightables is dead",()=>{
       const black: Fightable = {
         name:"ぶらっく",
@@ -115,6 +144,9 @@ describe("Battle",()=>{
       expect(() => battle.nextTurn()).toThrow(); // throws error if nextTurn is called after battle has finished.
     });
 
+    /**
+    * 両方の[[Fightable]]が生存していれば戦闘を続行する
+    */
     it("will not finish battle if both of Fightables are alive",()=>{
       const black: Fightable = {
         name:"ぶらっく",
@@ -156,6 +188,9 @@ describe("Battle",()=>{
       expect(() => battle.nextTurn()).not.toThrow();
     });
 
+    /**
+    * 総ターン数が最大ターン数を上回れば戦闘を終了する
+    */
     it("will finish battle when number of turns reaches max turn",()=>{
       const maxTurn = IntValue.of(10);
       const black: Fightable = {
@@ -177,7 +212,7 @@ describe("Battle",()=>{
         attack:jest.fn().mockReturnValue([]),
         chooseCommand:jest.fn()
       }
-      
+
       const battle = new Battle({black,white,maxTurn});
 
       const log:CommandLog = [];
