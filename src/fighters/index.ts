9592988eaa70bd3b8139f24e14a5e4c5235ef26b
tetsuya-zama <tetsuya.zama@gmail.com>
2020-02-20 16:30:02 +0900

    Add comment to fighters/index.ts

--- src/fighters/index.ts
+++ src/fighters/index.ts
@@ -2,15 +2,29 @@ import {Fightable} from '../core';
 import {Fighter ,FighterStatusData, isFighterStatusData, parseFighterStatus} from './fighter';
 import {Chicken} from './chicken';
 
+/**
+* [[Fightable]]作成要求のデータ型
+*/
 export interface FightableCreationRequest extends FighterStatusData{
+  /** 作成したい[[Fightable]]の型*/
   __type: string
 }
 
+/**
+* [[FightableCreationRequest]]のtype guard
+* @param 判定したいオブジェクト
+* @return objが[[FightableCreationRequest]]であればtrue
+*/
 export const isFighterCreationRequest = (obj: any): obj is FightableCreationRequest => {
   return typeof obj.__type === "string"
     && isFighterStatusData(obj);
 }
 
+/**
+* [[Fightable]]のインスタンスを作成する
+* @param obj 入力データ(JSONやYAMLなどの読み込み値)
+* @return 作成された[[Fightable]]
+*/
 export const createFighter = (obj: any): Fightable =>{
   if(isFighterCreationRequest(obj)){
     const status = parseFighterStatus(obj);
