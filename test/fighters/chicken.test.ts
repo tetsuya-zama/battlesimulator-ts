11a63d355e4bdbbe6769ce24e9d011a328c5ef87
tetsuya-zama <tetsuya.zama@gmail.com>
2020-02-20 17:39:28 +0900

    add comment to fighters/chicken.test.ts

--- test/fighters/chicken.test.ts
+++ test/fighters/chicken.test.ts
@@ -1,7 +1,15 @@
 import {Chicken} from '../../src/fighters/chicken';
 import {IntValue, COMMAND } from '../../src/core';
 
+/**
+* Chickenクラスに関するテスト
+* Fighterの継承クラスなのでコンストラクタと
+* overrideしているchooseCommandメソッドのみテストする
+*/
 describe("Chiken",() => {
+  /**
+  * (Fighterと同じく)FighterStatusによって作成される
+  */
   it("will be created with FighterStatus",()=>{
     const result = new Chicken({
       name:"ちきん",
@@ -14,7 +22,13 @@ describe("Chiken",() => {
     expect(result.name).toBe("ちきん");
   });
 
+  /**
+  * chooseCommandメソッドについてのテスト
+  */
   describe("chooseCommand",()=>{
+    /**
+    * 10%の確率で攻撃が選択される
+    */
     it("chooses ATTACK or GUARD for 10%:90% each",()=>{
       const fighter = new Chicken({
         name:"ちきん",
@@ -22,6 +36,10 @@ describe("Chiken",() => {
         offence:IntValue.of(8),
         deffence:IntValue.of(3)
       });
+
+      /**
+      * 1000回試行して攻撃が出る確率が10%に近似するかテストする
+      */
 
       const commands:COMMAND[] = [];
       for(var i=0; i<1000; i++){
