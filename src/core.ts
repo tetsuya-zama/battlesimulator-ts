a2b670babb7f8d031e0506bf8b1eb8c1aa54565f
tetsuya-zama <tetsuya.zama@gmail.com>
2020-02-20 14:05:37 +0900

    Add comment to core.ts

--- src/core.ts
+++ src/core.ts
@@ -1,18 +1,42 @@
+/**
+* [[Fightable]]の取る行動
+*/
 export enum COMMAND{
+  /**攻撃*/
   ATTACK,
+  /**防御*/
   GUARD
 }
 
+/**
+* ValueObjectの共通インタフェース
+* @typeparam T 実体の型
+*/
 export interface Value<T>{
+  /** ValueObjectの実体 */
   readonly value:T
 }
 
+/**
+* Int(整数)のValueObject
+* javascript(typescript)は数を表すnumber型はあるが整数型が無いので、util的な観点でValueObjectを作成した
+*/
 export class IntValue implements Value<number>{
+  /**
+  * 実体の数値
+  */
   private _value: number;
+  /**
+  * 実体を返す
+  */
   get value(){
     return this._value;
   }
-
+  /**
+  * コンストラクタ
+  * 与えられたvalueが整数でなければエラーをthrowする
+  * @param value 実体（整数であるべき）
+  */
   protected constructor(value: number){
     if(Math.floor(value) === value){
       this._value = value;
@@ -21,34 +45,112 @@ export class IntValue implements Value<number>{
     }
   }
 
+  /**
+  * IntValueを作成する
+  * @usage
+  * ```ts
+  * const intNum = IntValue.of(8);
+  * console.log(intNum.value);//-> "8"
+  * const invalidIntNum = IntValue.of(8.1); //throws Error
+  * ```
+  * @param n 整数もしくはnumberのValueObject
+  */
   static of(n: number | Value<number>):IntValue{
     return typeof n === "number" ? new IntValue(n) : new IntValue(n.value);
   }
 }
 
+/**
+* [[Battle]]の実行結果
+* 実体はstringの配列
+* @usage
+* ```ts
+* while(!battle.isFinished{
+*     const log:CommandLog = battle.nextTurn();
+*     // 1行分ずつログが格納されている
+*     log.forEach(line => console.log(line));
+* }
+* ```
+*/
 export type CommandLog = string[]
 
+/**
+* バトルを行うことができるオブジェクトのinterface
+*/
 export interface Fightable{
+  /**名前*/
   readonly name: string
+  /**生存中かどうか*/
   readonly alive: boolean
+  /**現在の状態を[[CommandLog]]として返す*/
   describeCurrentStatus():CommandLog
+  /**自身が今選択している[[COMMAND]]を返す*/
   getCurrentCommand(): COMMAND
+  /**
+  * 攻撃を受ける
+  * @param damage 与えたいダメージ
+  * @return 自身の防御力を加味した最終ダメージ(負の数なら回復)
+  */
   hit(damage: IntValue): IntValue
+  /**
+  * 引数に与えられた[[Fightable]]を攻撃する
+  * @param 攻撃される[[Fightable]]
+  * @return 攻撃結果[[CommandLog]]
+  */
   attack(enemy: Fightable): CommandLog
+  /**
+  * [[COMMAND]]を選択する
+  */
   chooseCommand():void
 }
 
+/**
+* Battle(戦闘)オブジェクト
+* BattleSiulator全体のメインオブジェクト
+*/
 export class Battle{
+  /**
+  * 先手
+  */
   private _black: Fightable;
+  /**
+  * 後手
+  */
   private _white: Fightable;
+  /**
+  * 現在のターン
+  */
   private _turn: number = 0;
+  /**
+  * 最大ターン
+  */
   private _maxTurn: number
+  /**
+  * 戦闘が終了しているかどうか
+  */
   private _isFinished: boolean = false;
 
+  /**
+  * 戦闘が終了しているかどうかを返す
+  * @useage
+  * ```ts
+  * //戦闘が終了するまでlogを出力しながら次のターンを実行する
+  * while(!battle.isFinished){
+  *    battle.nextTurn().forEach(l => console.log(l));
+  * }
+  * ```
+  * @return 終了していればtrue
+  */
   get isFinished(){
     return this._isFinished;
   }
 
+  /**
+  * コンストラクタ
+  * @param black 先手の[[Fightable]]
+  * @param white 後手の[[Fightable]]
+  * @param maxTurn 最大ターン
+  */
   constructor({black, white, maxTurn = IntValue.of(100)}
     :{black:Fightable, white:Fightable,maxTurn?:IntValue}){
     this._black = black;
@@ -56,6 +158,16 @@ export class Battle{
     this._maxTurn = maxTurn.value;
   }
 
+  /**
+  * 次のターンを実行する
+  * ```ts
+  * //戦闘が終了するまでlogを出力しながら次のターンを実行する
+  * while(!battle.isFinished){
+  *    battle.nextTurn().forEach(l => console.log(l));
+  * }
+  * ```
+  * @return 実行結果の[[CommandLog]]
+  */
   public nextTurn(): CommandLog{
     const log:CommandLog = [];
 
@@ -85,13 +197,27 @@ export class Battle{
     return log;
   }
 
+  /**
+  * 引き分けかどうか
+  * @return 引き分けならtrue
+  */
   private isDraw():boolean{
     return this._turn > this._maxTurn;
   }
+
+  /**
+  * 戦闘を終了させる
+  */
   private finish(){
     this._isFinished = true;
   }
 
+  /**
+  * 第１引数に設定した[[Fightable]]のコマンドを実行する
+  * @param me コマンドを実行する[[Fightable]]
+  * @param enemy コマンドが「攻撃」のとき攻撃される[[Fightable]]
+  * @return 実行結果の[[CommandLog]]
+  */
   private doCommand(me: Fightable, enemy: Fightable): CommandLog{
     if(me.getCurrentCommand() == COMMAND.ATTACK){
       const log: CommandLog = [];
@@ -109,6 +235,4 @@ export class Battle{
       return [`${me.name}は身を守っている`];
     }
   }
-
-
 }
